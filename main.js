138068ec8c5460d818816c6d67213c115abd3bfa
Milo <milo@MacBookPro.fritz.box>
2026-03-05 23:13:10 +0100

    t

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var swf_url = "SSF2BetaWindows.v.1.4.0.1.portable/SSF2Portable.swf"
+var path = "SSF2BetaWindows.v.1.4.0.1.portable/"
 window.RufflePlayer = window.RufflePlayer || {};
 window.addEventListener("load", (event) => {
+    window.RufflePlayer.config = {
+        ...window.RufflePlayer.config,
+        polyfills: true,
+        base: path,
+        
+    }
     const ruffle = window.RufflePlayer.newest();
     const player = ruffle.createPlayer();
     // player.style.width = "600px"; 
     // player.style.height = "400px";
-    const container = document.getElementById("container");
+    const container = document.getElementById("ruffle_container");
     container.appendChild(player);
     player.ruffle().load(swf_url);
 });
